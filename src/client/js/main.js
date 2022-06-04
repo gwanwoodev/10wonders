fcc865e8698ec1aa3a373f6f59063dbe76eb4b6c
elcode <gwanwoodev@kakao.com>
2022-06-04 22:22:56 +0900

    main__section publihsing done, TODO: mixins

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -5,3 +5,7 @@ import "../scss/styles.scss";
 import "../images/cart_icon.png";
 import "../images/logo.png";
 import "../images/main_background01.jpg";
+import "../images/right_arrow.png";
+import "../images/mail_icon.png";
+import "../images/phone_icon.png";
+import "../images/scroll_icon.png";

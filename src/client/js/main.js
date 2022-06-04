a42b5f9fd35ad337d3ef358771624f9ce33205c8
elcode <gwanwoodev@kakao.com>
2022-06-05 00:17:51 +0900

    scroll section 80% done

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -9,3 +9,6 @@ import "../images/right_arrow.png";
 import "../images/mail_icon.png";
 import "../images/phone_icon.png";
 import "../images/scroll_icon.png";
+import "../images/scroll_back01.jpg";
+import "../images/scroll_back02.jpg";
+import "../images/scroll_back03.jpg";

e4be9a6b387cd827d13d815fea742984a0988c36
gwanwoodev <gwanwoodev@kakao.com>
2022-06-11 02:43:36 +0900

    Scroll bug fix

--- src/client/js/home.js
+++ src/client/js/home.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const accountPopupCloseBtn = document.querySelector(".account__close--icon");
 
     const $sections = $(".wheelSection");
-    const wheelSections = document.querySelectorAll(".wheelSection");
+    const header = document.querySelector(".header");
 
     let currentIndex = 0;
     let isAnimating = false; 
@@ -68,16 +68,17 @@ document.addEventListener("DOMContentLoaded", () => {
             if (!topIsReached($currentSection)) return;
             currentIndex--;
 
-
             let $previousSection = $($sections[currentIndex]);
             let offsetTop = $previousSection.offset().top;
             event.preventDefault();
             isAnimating = true;
             $("html, body").animate({scrollTop: offsetTop}, 1000, stopAnimation);
+        }
 
-
-
-            
+        if(currentIndex >= 1) {
+            header.style.backgroundColor = "black";
+        }else if(currentIndex === 0) {
+            header.style.backgroundColor = "transparent";
         }
     }, {passive: false});
 
@@ -113,7 +114,6 @@ document.addEventListener("DOMContentLoaded", () => {
         const infoObj = [
             {
                 image: "/static/images/main_background01.jpg",
-                headerBg: "transparent",
                 headText1: 'the best solar energy',
                 headText2: 'equipments from Korea',
                 subText1: "We provide the world's heighest quality solar energy equipments from variouis brands.",
@@ -121,7 +121,6 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             {
                 image: "/static/images/main_background02.jpg",
-                headerBg: "transparent",
                 headText1: 'Let the sun work for you',
                 headText2: 'We ship all over the world',
                 subText1: "We only carry the most profitable solar energy equipment.",
@@ -129,7 +128,6 @@ document.addEventListener("DOMContentLoaded", () => {
             },
             {
                 image: "/static/images/main_background03.jpg",
-                headerBg: "transparent",
                 headText1: 'We provide the easiest way to buy',
                 headText2: 'solar power equipment',
                 subText1: "Order, Pay and Get, Go through only 3 steps.",
@@ -143,7 +141,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         mainSection.style.backgroundImage = `url('${infoObj[index].image}')`;
-        headerElement.style.backgroundColor = `${infoObj[index].headerBg}`;
         mainSectionHeads[0].innerHTML = `${infoObj[index].headText1}`
         mainSectionHeads[1].innerHTML = `${infoObj[index].headText2}`
         mainSectionSubs[0].innerHTML = `${infoObj[index].subText1}`
